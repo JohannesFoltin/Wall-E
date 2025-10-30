cd6f45dad8a93965ad501f114783b8ec5d130098
Kai <kad7@web.de>
2025-10-30 18:24:20 +0100

    eyy

--- sensor copy.py
+++ sensor copy.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+import sys
 from time import sleep
-from ev3dev2.sensor import INPUT_1, INPUT_4
+from ev3dev2.sensor import INPUT_3, INPUT_4
 from ev3dev2.sensor.lego import LightSensor, ColorSensor
 
-ls_1 = LightSensor(INPUT_1)
+ls_1 = LightSensor(INPUT_3)
 ls_2 = ColorSensor(INPUT_4)
 
 
