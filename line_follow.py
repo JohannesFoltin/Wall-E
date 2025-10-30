3ac3e751be593349d15d0e2ff85b6a8d4e825845
Kai <kad7@web.de>
2025-10-30 13:27:16 +0100

    line_follow

---
+++ line_follow.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from time import sleep
+from ev3dev2.sensor import INPUT_1,INPUT_2
+from ev3dev2.sensor.lego import LightSensor, ColorSensor
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B
+from ev3dev2.motor import LargeMotor, SpeedPercent
+
+drive_motor = LargeMotor(OUTPUT_A)
+control_motor = LargeMotor(OUTPUT_B)
+steer_ls_l = LightSensor(INPUT_1)
+steer_ls_r = LightSensor(INPUT_2)
+
+
+
+
+
+def follow_line():
+    drive_motor.SpeedPercent(100)
+    while True:
+        if 
+    
+
+follow_line()
+'''
+wall-e.on_for_rotations(SpeedPercent(100),5)
+
+    SpeedDPS(degrees_per_second, desc=None)
+    SpeedPercent(percent, desc=None)
+    ls = LightSensor(INPUT_1) # oder 'ColorSensor(INPUT_1)'
+print(ls.reflected_light_intensity)
+print(ls.ambient_light_intensity)
+sleep(3)
+
+'''    
