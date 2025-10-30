57fa1465f93a75d1dc335190f9194865b58cf73e
Kai <kad7@web.de>
2025-10-30 16:34:42 +0100

    follow line

--- line_follow.py
+++ line_follow.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from time import sleep
+from math import tanh
 from ev3dev2.sensor import INPUT_1, INPUT_4
 from ev3dev2.sensor.lego import LightSensor
 from ev3dev2.motor import OUTPUT_A, OUTPUT_B
@@ -12,19 +13,25 @@ steer_ls_r = LightSensor(INPUT_4)
 
 
 def follow_line():
+    tt = 0.75  # turning time
+    tm1 = 2  # turning multiplikator 1
+    tm2 = 2  # turning multiplikator 2
+    error_rate = 0
     drive_motor.on(SpeedPercent(20))
     while True:
         ref1 = steer_ls_l.reflected_light_intensity
         ref2 = steer_ls_r.reflected_light_intensity
+        error = ref1 - ref2
         if ref1 > ref2:
+            control_motor.on_for_degrees(45 * tanh(tm1 * error + tm2 * error_rate ), tt)  # oder arctan
+            control_motor.on_for_degrees(-45 * tanh(tm * error), tt)
+
             print("DEINE MUDDA IST FETTT")
-            print("is_1: ", steer_ls_l.reflected_light_intensity)
-            print("is_1: ", steer_ls_l.ambient_light_intensity)
-            print("is_2: ", steer_ls_r.reflected_light_intensity)
-            print("is_2: ", steer_ls_r.ambient_light_intensity)
         elif ref1 < ref2:
+            control_motor.on_for_degrees(-45 * tanh(tm * error), tt)
+            control_motor.on_for_degrees(45 * tanh(tm * error), tt)
             print("DEINE MUTTER IST RECHT SPORTLICH")
-        sleep(1)
+        sleep(0.1)
 
 
 follow_line()
