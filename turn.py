264fcab7d6a68ff6a0815ce82b3bf0ef302359c9
Kai <kad7@web.de>
2025-11-02 18:52:39 +0100

    sd

--- turn.py
+++ turn.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 import time
-from ev3dev2.sensor import INPUT_1, INPUT_4, INPUT_2
-from ev3dev2.sensor.lego import LightSensor, UltrasonicSensor
+from ev3dev2.sensor import INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import LightSensor, ColorSensor, UltrasonicSensor
 from ev3dev2.motor import OUTPUT_A, OUTPUT_B
 from ev3dev2.motor import LargeMotor, SpeedPercent
 
-drive_motor = LargeMotor(OUTPUT_A)
-control_motor = LargeMotor(OUTPUT_B)
-steer_ls_l = LightSensor(INPUT_1)
-steer_ls_r = LightSensor(INPUT_4)
+drive_motor = LargeMotor(OUTPUT_B)
+control_motor = LargeMotor(OUTPUT_A)
+steer_ls_r = LightSensor(INPUT_3)
+steer_ls_l = ColorSensor(INPUT_4)
 u_distance = UltrasonicSensor(INPUT_2)
 
 
 def turn():
     # if linie weg | abstand 20cm
-    turn_time: float = 2.0
-    turn_angle: float = -60.0
-    min_distance: float = 5
+    turn_time = 2.0
+    turn_angle = -60.0
+    min_distance = 5
     while True:
         drive_motor.off()
         turn_angle *= -1
@@ -29,4 +29,5 @@ def turn():
             elif u_distance.distance_centimeters_ping < min_distance:
                 break
 
-# Sensoren vorne bzw hinten können die dauer (2) ersetzen
+
+turn()
