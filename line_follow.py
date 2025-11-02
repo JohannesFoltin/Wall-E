100921691bc6987f018d9e689dcad41d258a4bc5
Kai <kad7@web.de>
2025-11-02 16:32:11 +0100

    d

--- line_follow.py
+++ line_follow.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 from time import sleep, time
 from math import tanh
-from ev3dev2.sensor import INPUT_1, INPUT_4
+from ev3dev2.sensor import INPUT_3, INPUT_4
 from ev3dev2.sensor.lego import LightSensor
 from ev3dev2.motor import OUTPUT_A, OUTPUT_B
 from ev3dev2.motor import LargeMotor, SpeedPercent
 
 drive_motor = LargeMotor(OUTPUT_A)
 control_motor = LargeMotor(OUTPUT_B)
-steer_ls_l = LightSensor(INPUT_1)
+steer_ls_l = LightSensor(INPUT_3)
 steer_ls_r = LightSensor(INPUT_4)
 
 
