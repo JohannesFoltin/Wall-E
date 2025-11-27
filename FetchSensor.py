53b8abd6b9d0a3b1e01c13c4c22f8773f03b6a87
Johannes <Johannes.Foltin@gmx.de>
2025-11-27 10:08:10 +0100

    Update Sensor

--- FetchSensor.py
+++ FetchSensor.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python3
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
-from ev3dev2.sensor.lego import LightSensor, ColorSensor, UltrasonicSensor
+from ev3dev2.sensor.lego import LightSensor, UltrasonicSensor
 
 u_distance = UltrasonicSensor(INPUT_1)
 ls_r = LightSensor(INPUT_2)  # rechter Sensor auf Input 2
-ls_c = ColorSensor(INPUT_3)  # center Sensor auf Input 3 # neuer Sensor
+ls_c = LightSensor(INPUT_3)  # center Sensor auf Input 3 # neuer Sensor
 ls_l = LightSensor(INPUT_4)  # links Sensor auf Input 4
+
+def init_threshold() -> tuple(int,int):
+    light_ping_l = ls_l.reflected_light_intensity
+    light_ping_c = ls_c.reflected_light_intensity
+    light_ping_r = ls_r.reflected_light_intensity
+
+    white = (light_ping_l + light_ping_r) / 2
+    black = light_ping_c
+
+    return (white,black)
+
+def update_threshold(old_values) -> tuple(int,int):
+    light_ping_l = ls_l.reflected_light_intensity
+    light_ping_c = ls_c.reflected_light_intensity
+    light_ping_r = ls_r.reflected_light_intensity
+
+    white = old_values[0]
+    black = old_values[1]
+    
+    if light_ping_l > white:
+        white = light_ping_l
+    elif light_ping_l < black:
+        black = light_ping_l
+    if light_ping_c > white:
+        white = light_ping_c
+    elif light_ping_c < black:
+        black = light_ping_c
+    if light_ping_r > white:
+        white = light_ping_r
+    elif light_ping_r < black:
+        black = light_ping_r
+    
+    return (white,black)
 
 
 def fetch_sensor():
